99ee67b2041541fa425faf079d7784f5a59fb5f0
scapula07 <bartholomewonogwu@yahoo.com>
2023-03-12 15:25:17 +0100

    fix query category

--- src/components/inputFields/SelectQueryCate/index.js
+++ src/components/inputFields/SelectQueryCate/index.js
@@ -4,7 +4,7 @@ export default function SelectQueryCate() {
   return (
     <div className='w-full rounded-full border flex items-center px-4 py-4 space-x-2'>
    
-     <select placeholder='' style={{color: "#989B9E"}} className="outline-none w-full" >
+     <select placeholder='' style={{color: "#989B9E"}} className="outline-none w-full bg-white" >
             <option value="volvo" className='outline-none w-full'>Select query category</option>
      </select>
      </div>
